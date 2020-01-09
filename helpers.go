3877123b6558d0a726c2546b89a148e7d685604d
0x0bsod <zlodey23@gmail.com>
2020-01-09 12:28:34 +0300

    Init

---
+++ helpers.go
@@ -0,0 +1,23 @@
+package goLittleHelpers
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return err
+}
