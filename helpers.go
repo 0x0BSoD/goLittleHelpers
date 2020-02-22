2b3815bf2cccbbf63770ac246f4471989dbba56b
0x0bsod <zlodey23@gmail.com>
2020-02-22 23:35:39 +0300

    Add converter

--- helpers.go
+++ helpers.go
@@ -3,6 +3,8 @@ package goLittleHelpers
 import (
 	"encoding/json"
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func StringInSlice(a string, list []string) bool {
@@ -20,4 +22,58 @@ func PrettyPrint(v interface{}) (err error) {
 		fmt.Println(string(b))
 	}
 	return err
+}
+
+func round(val float64, roundOn float64, places int) (newVal float64) {
+	var round float64
+	pow := math.Pow(10, float64(places))
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	newVal = round / pow
+	return
+}
+
+type CovertItem int
+
+const (
+	Speed CovertItem = iota
+	Size
+	Memory
+)
+
+// ConvertBytes get size in bytes(type float64) and return rounded,
+// converted size as formatted string
+func ConvertBytes(in float64, i CovertItem) string {
+
+	if in <= 0.0 {
+		return "0 B"
+	}
+
+	var suffixes [5]string
+
+	switch i {
+	case Speed:
+		suffixes = [5]string{"b/s", "kB/s", "MB/s", "GB/s", "TB/s"}
+	case Size:
+		suffixes = [5]string{"B", "kB", "MB", "GB", "TB"}
+	case Memory:
+		suffixes = [5]string{"B", "KiB", "MiB", "GiB", "TiB"}
+	default:
+		return "0 B"
+	}
+
+	base := math.Log(in) / math.Log(1024)
+	getSize := round(math.Pow(1024, base-math.Floor(base)), .5, 2)
+	s := int(math.Floor(base))
+	if s <= 5 {
+		getSuffix := suffixes[s]
+		return strconv.FormatFloat(getSize, 'f', -1, 64) + " " + getSuffix
+	} else {
+		return "0 B"
+	}
 }
